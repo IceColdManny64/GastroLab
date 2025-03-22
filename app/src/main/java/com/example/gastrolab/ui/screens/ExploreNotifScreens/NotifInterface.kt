47843d87229ccbbb03c370199a35a7a52cc92b4d
Ailyn06-hh <22151242@aguascalientes.tecnm.mx>
2025-03-22 15:47:59 -0600

    "NotiInterface Commit Add Screen"

--- app/src/main/java/com/example/gastrolab/ui/screens/ExploreNotifScreens/NotifInterface.kt
+++ app/src/main/java/com/example/gastrolab/ui/screens/ExploreNotifScreens/NotifInterface.kt
@@ -31,12 +31,9 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NotifInterface(navController: NavHostController) {
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    fun NotifInterface(navController: NavHostController) {
         Scaffold(
             topBar = {
                 TopAppBar(
@@ -148,4 +145,3 @@ fun NotifInterface(navController: NavHostController) {
             }
         }
     }
-}
