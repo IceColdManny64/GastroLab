96784769ab9fffa9a64a96295ff6fd5c3d88fb24
JOSÉ MANUEL JUÁREZ DÍAZ <22151192@aguascalientes.tecnm.mx>
2025-03-23 19:02:19 -0600

    SettingsInterface.kt demo is done, navigation implemented to switch to screens located in TroubleShootScreen package

--- app/src/main/java/com/example/gastrolab/ui/screens/MainScreens/MainScreen.kt
+++ app/src/main/java/com/example/gastrolab/ui/screens/MainScreens/MainScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
@@ -162,7 +163,6 @@ fun Bars(navController: NavHostController) {
 
 @Composable
 fun Adaptive(navController: NavHostController) {
-    var windowSize = currentWindowAdaptiveInfo().windowSizeClass
     var height = currentWindowAdaptiveInfo().windowSizeClass.windowHeightSizeClass
     var width = currentWindowAdaptiveInfo().windowSizeClass.windowWidthSizeClass
 
--- app/src/main/java/com/example/gastrolab/ui/screens/MainScreens/SettingsInterface.kt
+++ app/src/main/java/com/example/gastrolab/ui/screens/MainScreens/SettingsInterface.kt
@@ -1,216 +1,249 @@
 package com.example.gastrolab.ui.screens.MainScreens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Apps
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.LockPerson
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.ReportProblem
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.SupportAgent
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.window.core.layout.WindowWidthSizeClass
+import com.example.clasetrabajo.data.model.MenuModel
 import com.example.gastrolab.R
+import com.example.gastrolab.data.model.SettingsModel
+import com.example.gastrolab.ui.components.MainViewExCard
 import com.example.gastrolab.ui.screens.TroubleshootScreens.Report
 
 @Composable
 fun SettingsInterface(navController: NavHostController) {
     BarsSettings(navController)
-
-    Column {
-        Button(
-            onClick = {navController.navigate("reportProblem")}
-        )
-        {
-            Text("Go to Report Screen")
-        }
-        Button(
-            onClick = {navController.navigate("supportScreen")}
-        )
-        {
-            Text("Go to Support Screen")
-        }
-    }
-
-
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 
-//@Preview
-
 @Composable
 
 fun BarsSettings(navController: NavHostController) {
-
-
     Column(
 
         modifier = Modifier
 
-            .fillMaxSize()
-
+            .fillMaxSize(),
 
     ) {
 
 //can use MediumTopAppBar and other similar components to change the top bar size.
 
         TopAppBar(
-
-            modifier = Modifier
-
-                .height(50.dp),
-
+            modifier = Modifier.height(50.dp),
             colors = TopAppBarDefaults.topAppBarColors(
-
                 containerColor = MaterialTheme.colorScheme.primary,
-
                 titleContentColor = MaterialTheme.colorScheme.secondary
-
             ),
-
             title = {
-
                 val gastroGradient = listOf(
-                    MaterialTheme.colorScheme.surface, MaterialTheme.colorScheme.tertiary,
-
-                    MaterialTheme.colorScheme.onSurface,
-
-                    MaterialTheme.colorScheme.onTertiary
+                    MaterialTheme.colorScheme.tertiary,
+                    MaterialTheme.colorScheme.surface,
+                    MaterialTheme.colorScheme.tertiary
                 )
-
                 Text(
-
-                    text = stringResource(R.string.app_name),
-
-                    style = TextStyle(brush = Brush.linearGradient(colors = gastroGradient)),
-
-                    color = MaterialTheme.colorScheme.secondary,
-
+                    text = stringResource(R.string.settings_header),
+                    style = TextStyle(brush = Brush.verticalGradient(colors = gastroGradient)),
                     fontStyle = FontStyle.Italic,
-
                     fontWeight = FontWeight.ExtraBold,
-
                     fontSize = 25.sp
-
                 )
             },
-
             actions = {
-                IconButton(onClick = {}) {
-                    Icon(
-                        imageVector = Icons.Filled.AccountCircle,
-                        contentDescription = "Account icon"
-                    )
+                IconButton(onClick = { navController.navigate("accountScreen") }) {
+                    Icon(imageVector = Icons.Filled.AccountCircle, contentDescription = "Account icon")
                 }
             }
         )
         Column(
 
             modifier = Modifier
-
                 .weight(1f)
-
                 .fillMaxSize()
-
                 .background(MaterialTheme.colorScheme.background)
 
-
         ) {
-
+            SettingsAdaptive(navController)
         }
 
         BottomAppBar(
-
             modifier = Modifier
-
                 .fillMaxWidth()
-
-                .height(50.dp)
-
-                .background(MaterialTheme.colorScheme.primary),
-
+                .height(50.dp),
             containerColor = MaterialTheme.colorScheme.primary,
-
             contentColor = MaterialTheme.colorScheme.onPrimary
-
-
         ) {
-
-            IconButton(
-
-                modifier = Modifier
-
-                    .weight(1f),
-
-                onClick = { navController.navigate("mainScreen") },
-
-                ) {
-
+            IconButton(modifier = Modifier.weight(1f), onClick = { navController.navigate("mainScreen") }) {
                 Icon(imageVector = Icons.Filled.Home, contentDescription = "")
-
             }
-
-            IconButton(
-
-                modifier = Modifier
-
-                    .weight(1f),
-
-                onClick = { navController.navigate("searchScreen") },
-
-                ) {
-
+            IconButton(modifier = Modifier.weight(1f), onClick = { navController.navigate("searchScreen") }) {
                 Icon(imageVector = Icons.Filled.Search, contentDescription = "")
-
             }
-
-            IconButton(
-
-                modifier = Modifier
-
-                    .weight(1f),
-
-                onClick = { navController.navigate("notifScreen") },
-
-                ) {
-
+            IconButton(modifier = Modifier.weight(1f), onClick = { navController.navigate("notifScreen") }) {
                 Icon(imageVector = Icons.Filled.Notifications, contentDescription = "")
-
             }
-
-            IconButton(
-
-                modifier = Modifier
-
-                    .weight(1f),
-
-                onClick = { navController.navigate("settingsScreen") },
-
-                ) {
-
+            IconButton(modifier = Modifier.weight(1f), onClick = { navController.navigate("settingsScreen") }) {
                 Icon(imageVector = Icons.Filled.Menu, contentDescription = "")
+            }
+        }
+    }
+}
 
+@Composable
+fun SettingsAdaptive(navController: NavHostController) {
+    var height = currentWindowAdaptiveInfo().windowSizeClass.windowHeightSizeClass
+    var width = currentWindowAdaptiveInfo().windowSizeClass.windowWidthSizeClass
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+
+    ) {
+
+
+        val settingsArray = arrayOf(
+            SettingsModel(1, "Configuración de Aplicación", "Realiza cambios a tu cuenta, " +
+                    "revisa opciones de notificaciones y accesibilidad", Icons.Filled.Apps),
+            SettingsModel(2, "Privacidad y seguridad", "Cambia tu contraseña, borra tu cuenta o" +
+                    " gestiona tu información", Icons.Filled.LockPerson),
+            SettingsModel(3, "Soporte técnico", "Entra a un chat automático, o contacta a un experto para" +
+                    "resolver un problema en la app", Icons.Filled.SupportAgent),
+            SettingsModel(4, "Reporta un problema", "Reporta un error o problema general en la aplicacion." +
+                    "Nos ayuda a mejorar!", Icons.Filled.ReportProblem)
+        )
+
+        if (width == WindowWidthSizeClass.COMPACT) {
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(minSize = 200.dp),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1.1f),
+
+                verticalArrangement = Arrangement.Center,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                items(settingsArray) { item ->
+                    SettingsList(item.id, item.title, item.text, item.icon, navController)
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SettingsList(id: Int, title: String, text: String, icon: ImageVector, navController: NavHostController) {
+    val navArray = arrayOf(
+        "userMenuScreen",
+        "privacyScreen",
+        "supportScreen",
+        "reportProblem"
+    )
+
+    // Verifica que el ID sea válido dentro del array
+    val destination = if (id in 1..navArray.size) navArray[id - 1] else null
+
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+            .clickable {
+                destination?.let { navController.navigate(it) }
+            }
+            .border(
+                shape = RoundedCornerShape(10.dp),
+                width = 3.dp,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = "icon",
+                tint = MaterialTheme.colorScheme.onBackground,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .size(40.dp)
+            )
+            Column(
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .fillMaxSize()
+            ) {
+                Text(
+                    text = title,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+                Text(
+                    text = text,
+                    fontSize = 15.sp,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
             }
         }
     }
